2f4dde0cff829f055040b74d91bb0c9a793cb2f5
80at8 <48815854+80at8@users.noreply.github.com>
2020-04-11 03:31:53 -0600

    Update FreeRadiusSetup.php

--- src/FreeRadiusSetup.php
+++ src/FreeRadiusSetup.php
@@ -35,7 +35,7 @@ class FreeRadiusSetup
                 CommandExecutor::executeCommand("/bin/cp " . __DIR__ . $c . "sql /etc/freeradius/3.0/mods-available");
                 CommandExecutor::executeCommand("/bin/ln -s /etc/freeradius/3.0/mods-available/sql -t /etc/freeradius/3.0/mods-enabled/");
                 CommandExecutor::executeCommand("/bin/cp " . __DIR__ . $c . "default /etc/freeradius/3.0/sites-available/");
-                CommandExecutor::executeCommand("/bin/ln -s /etc/freeradius/3.0/sites-available/default -t /etc/freeradius/3.0/sites-enabled/")
+                CommandExecutor::executeCommand("/bin/ln -s /etc/freeradius/3.0/sites-available/default -t /etc/freeradius/3.0/sites-enabled/");
                 CommandExecutor::executeCommand("/bin/sed -i 's/password = \"radpass\"/password = \"$mysqlPassword\"/g' /etc/freeradius/3.0/mods-enabled/sql");
                 CommandExecutor::executeCommand("/usr/sbin/service freeradius restart");
             }
